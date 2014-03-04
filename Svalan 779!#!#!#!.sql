555c3bc4cf065a80b240c62e6cb605009693bbfe
kozniii <keoalosh@gmail.com>
2014-03-04 23:08:56 +0100

    matlista
    veckans mat (99)

---
+++ Svalan 779!#!#!#!.sql
@@ -0,0 +1,28 @@
+-- create table
+CREATE TABLE cities
+(
+  	id INT PRIMARY KEY NOT NULL auto_increment,
+	dag VARCHAR(255),
+	mat VARCHAR(255),
+	vegetarisk VARCHAR(255),
+	glasyr VARCHAR(255)
+);
+
+-- Insert data into our table
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
+
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
+
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
+
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
+
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
+
+INSERT INTO cities(dag, mat, vegetarisk, glasyr)
+VALUE ('Måndag', 'biff', ärta, 'chokolate');
